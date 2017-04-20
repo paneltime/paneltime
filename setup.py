fae78397e3e611a20b1ded6aa6cd8965e38ed1ba
espensirnes <espen.sirnes@uit.no>
2017-04-20 10:22:07 +0200

    new build

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.0.1',
 
     description='An efficient integrated panel and GARCH estimator',
     long_description=long_description,
@@ -47,7 +47,7 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Researchers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Statistical Software :: time series estimation',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: GPL-3.0 License',
