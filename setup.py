ad2cd87294e59ae40b5b157e6f590b39bd3305f2
espensirnes <espen.sirnes@uit.no>
2017-09-17 15:46:18 +0200

    testing

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     #**************************************************************************REMOVED>
-    install_requires=['numpy >= 1.11','scipy','matplotlib','cfunctions'],
+    install_requires=['numpy >= 1.11','scipy','matplotlib'],
     #**************************************************************************<REMOVED
 
     # List additional groups of dependencies here (e.g. development
