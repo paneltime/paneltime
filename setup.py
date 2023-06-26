4dc92c655754a5b4fd9a6f994df79cb67f412029
espensirnes <espen.sirnes@uit.no>
2023-06-26 19:16:35 +0200

    New version 1.2.35 committed: removed psutil since it reqiures vs14 to build

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-version='1.2.34'
+version='1.2.35'
 
 from setuptools import setup, find_packages
 from codecs import open
@@ -44,7 +44,7 @@ setup(
 
   packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
-  install_requires=['numpy >= 1.11','pymysql', 'pandas', 'psutil', 'mpmath'],
+  install_requires=['numpy >= 1.11','pymysql', 'pandas',  'mpmath'],
 	extras_require={'linux':'gcc'},	
 
   package_data={
